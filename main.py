3fa0da04cff280a76766f1650b5af35f6789329d
Roa-Haskel <Roa-Haskel@outlook.com>
2022-10-13 00:28:43 +0800

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pynput import mouse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+from pynput.mouse import Controller
+
+mouse = Controller()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if not pressed:
+        # Stop listener
+        return False
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format(
+        'down' if dy < 0 else 'up',
+        (x, y)))
+
+# Collect events until released
+# with mouse.Listener(
+#         on_move=on_move,
+#         on_click=on_click,
+#         on_scroll=on_scroll) as listener:
+#     listener.join()
+
+
+
+# ...or, in a non-blocking fashion:
+listener = mouse.Listener(
+    suppress=True,
+    on_move=on_move,
+    on_click=on_click,
+    on_scroll=on_scroll)
+
+
+listener.start()
+listener.join()
---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+
+class Server:
+    server=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    server.bind(('0.0.0.0',8989))
+    server.connect(("8.8.8.8",80))
+    ip=server.getsockname()[0]
+    hosts=['192.168.99.132','192.168.43.207']
+    hosts.remove(ip)
+    target=(hosts[0],8989)
+    def send(self,data:str):
+        self.server.send(data.encode())
+    def recv(self):
+        return self.server.recv(10240).decode()
