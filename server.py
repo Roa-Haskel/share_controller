ad0b51ec9dc5e3dfd6b29a95861a2254dc117d30
Roa-Haskel <10317199+Roa-Haskel@user.noreply.gitee.com>
2022-10-19 21:38:20 +0800

    update manage server add mainloop

--- server.py
+++ server.py
@@ -25,49 +25,42 @@ class AbsServer:
 
 class CommonServer(AbsServer):
     MSAGE_SEP=bytes(bytearray([i+10 for i in 'common_server_msage_sep'.encode()]))
-    HEART_TYPE_LOGO=-1111111
     def __init__(self,port):
         super().__init__(port)
-        self.__msageContainer=[]
+        self.__lock=threading.Lock()
     def recvfrom(self):
         while True:
             data,addr=super().recvfrom()
             if self.MSAGE_SEP in data:
                 return data,addr
 
-
-
-    def msageParaser(self,dataTuple:tuple):
-        msageType, message = dataTuple[0].split(self.MSAGE_SEP)
+    def getMsage(self):
+        dataTuple=self.recvfrom()
+        msageType, message,addr = dataTuple[0].split(self.MSAGE_SEP)
         msageType = int(msageType)
-        return msageType,message,dataTuple[1]
+        return msageType, message, addr
 
-    def getMsage(self,inputMsageType:int)->tuple:
-        for index in range(len(self.__msageContainer)):
-            msgType,msage,addr=self.__msageContainer[index]
-            if msgType==inputMsageType:
-                del self.__msageContainer[index]
-                return msage,addr
-
-        while True:
-            msgType,msage,addr=self.msageParaser(self.recvfrom())
-            if msgType==inputMsageType:
-                return msage,addr
-            self.__msageContainer.append((msgType, msage, addr))
 
     def sendMsage(self,data:str,target:tuple,msgType:int=0):
         assert isinstance(msgType,int)
         if not isinstance(data,bytes):
             data=str(data).encode()
-        data=str(msgType).encode()+self.MSAGE_SEP+data
+        data=str(msgType).encode()+self.MSAGE_SEP+data+self.MSAGE_SEP+str(self.getLocalAddr()).encode()
         super().sendTo(data,target)
 
 class ManageServer(CommonServer):
 
+    class MsageType:
+        #心跳事件
+        HEART_TYPE_LOGO = -1111111
+        #控制器事件
+        CONTROLLER_EVENTS=1
+        #屏幕管理器事件
+        SCREEN_MANAGER_EVENTS=2
     def __init__(self,port=19999):
         super().__init__(port)
         self.clients=RemoveCallbackSet([],13)
-        for method in [self._chickHeatBeat,self._sendHeatBeat,self._sendToAllLan]:
+        for method in [self._eventMainLoop, self._sendHeatBeat, self._sendToAllLan]:
             threading.Thread(target=method).start()
 
     @methodForLoop(50,10)
@@ -79,18 +72,29 @@ class ManageServer(CommonServer):
             allIps=[topThree+"."+str(i) for i in range(1,255) if str(i)!=splitIp[-1]]
             for i in allIps:
                 try:
-                    self.sendMsage(self.getLocalAddr(), (i,self._port), self.HEART_TYPE_LOGO)
+                    self.sendMsage('', (i,self._port), self.HEART_TYPE_LOGO)
                 except:
                     print('send to %s error'%(str((ip,self._port))))
             time.sleep(1)
-    @methodForLoop(0)
-    def _chickHeatBeat(self):
-        msg, addr = self.getMsage(self.HEART_TYPE_LOGO)
-        self.clients.add(eval(msg))
     @methodForLoop(8,3)
     def _sendHeatBeat(self):
         for i in self.clients:
-            self.sendMsage(self.getLocalAddr(),i,self.HEART_TYPE_LOGO)
+            try:
+                self.sendMsage('',i,self.HEART_TYPE_LOGO)
+            except:
+                pass
+    @methodForLoop(0)
+    def _eventMainLoop(self):
+        msgType, msg, addr = self.getMsage()
+        if msgType == self.MsageType.HEART_TYPE_LOGO:
+            self.clients.add(addr)
+        elif msgType == self.MsageType.CONTROLLER_EVENTS:
+            pass
+        elif msgType == self.MsageType.SCREEN_MANAGER_EVENTS:
+            pass
+        else:
+            print("unknow msage type %s"%msgType)
+
 class ControllerServer(CommonServer):
     pass
 
