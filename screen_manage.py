68feaa26414f83e9f19ff09bedb6c8d538615733
Roa-Haskel <10317199+Roa-Haskel@user.noreply.gitee.com>
2022-10-20 22:33:57 +0800

    screen manage

--- screen_manage.py
+++ screen_manage.py
@@ -1,7 +1,23 @@
 
+class Screen:
+    def __init__(self,width:int,height:int,leftTop:tuple):
+        self.width=width
+        self.height=height
+        self.leftTop=leftTop
+    def isInscreen(self,coordinate:tuple):
+        return coordinate[0]>self.leftTop[0] and coordinate[0]<self.leftTop[0]+self.width \
+            and coordinate[1]>self.leftTop[1] and coordinate[1]<self.leftTop[1]+self.height
+    def reLeftTop(self,leftTop:tuple):
+        self.leftTop=leftTop
+
 class ScreenManage:
-    def __int__(self):
-        pass
-    def move(self,x,y):
-        if 1==1:
-            return target,x,y
+    def __int__(self,addr,width:int,height:int):
+        screen=Screen(width,height,(0,0))
+        self.screens={addr:screen}
+    def addClient(self,target:tuple,width:int,height:int):
+        self.screens[target]=Screen(width,height,(0,0))
+        screenkeysSort=sorted(self.screens.keys())
+        for screenKey,index in zip(screenkeysSort,range(len(self.screens))):
+            if index>0:
+                lastSc=self.screens[screenkeysSort[index-1]]
+                self.screens[screenKey].reLeftTop((lastSc.width+lastSc.leftTop[0],0))
