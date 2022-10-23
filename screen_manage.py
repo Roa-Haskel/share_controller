82f5aa2b8ee898b8bc89e4df6211144665212d9f
Roa Haskel's mac <10317199+Roa-Haskel@user.noreply.gitee.com>
2022-10-23 20:26:56 +0800

    screen manage chagne 20221021

--- screen_manage.py
+++ screen_manage.py
@@ -1,23 +1,37 @@
-
-class Screen:
-    def __init__(self,width:int,height:int,leftTop:tuple):
-        self.width=width
-        self.height=height
-        self.leftTop=leftTop
-    def isInscreen(self,coordinate:tuple):
-        return coordinate[0]>self.leftTop[0] and coordinate[0]<self.leftTop[0]+self.width \
-            and coordinate[1]>self.leftTop[1] and coordinate[1]<self.leftTop[1]+self.height
-    def reLeftTop(self,leftTop:tuple):
-        self.leftTop=leftTop
+import tkinter
 
 class ScreenManage:
-    def __int__(self,addr,width:int,height:int):
-        screen=Screen(width,height,(0,0))
-        self.screens={addr:screen}
-    def addClient(self,target:tuple,width:int,height:int):
-        self.screens[target]=Screen(width,height,(0,0))
-        screenkeysSort=sorted(self.screens.keys())
-        for screenKey,index in zip(screenkeysSort,range(len(self.screens))):
+    @staticmethod
+    def isInscreen(screen:tuple,coordinate):
+        ((width,height),leftTop)=screen
+        return coordinate[0] > leftTop[0] and coordinate[0] < leftTop[0] + width \
+               and coordinate[1] > leftTop[1] and coordinate[1] < leftTop[1] + height
+
+    def __init__(self,selfAddr:tuple):
+        self.selfAddr=selfAddr
+        win=tkinter.Tk()
+        self.screens={selfAddr:((win.winfo_screenwidth(),win.winfo_screenheight()),(0,0))}
+        win.destroy()
+
+    def addClient(self,target:tuple,screen:tuple):
+        self.screens[target]=screen
+        screenkeysSort = sorted(self.screens.keys())
+        for screenKey, index in zip(screenkeysSort, range(len(self.screens))):
             if index>0:
                 lastSc=self.screens[screenkeysSort[index-1]]
-                self.screens[screenKey].reLeftTop((lastSc.width+lastSc.leftTop[0],0))
+                # self.screens[screenKey].reLeftTop((lastSc.width+lastSc.leftTop[0],0))
+                screen=self.screens[screenKey]
+                self.screens[screenKey]=(screen[0],(lastSc[0][0]+lastSc[1][0],0))
+    def update(self,screens:dict=None):
+        if screens:
+            self.screens=screens
+        #以当前显示器左上角为原点重置所有显示器坐标
+        if self.screens[self.selfAddr][1]!=(0,0):
+            left,top=self.screens[self.selfAddr][1]
+            for key,screen in self.screens.items():
+                ((w,h),(l,t))=screen
+                self.screens[key]=((w,h),(l-left,top-top))
+
+
+
+
