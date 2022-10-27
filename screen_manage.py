96161424657887e5a9ef7d688fa2f72b75f3473d
Roa-Haskel <10317199+Roa-Haskel@user.noreply.gitee.com>
2022-10-28 00:34:11 +0800

    key event factory 8

--- screen_manage.py
+++ screen_manage.py
@@ -15,7 +15,7 @@ class ScreenManage:
         #        and coordinate[1] > leftTop[1] and coordinate[1] < leftTop[1] + height
         ((w,h),(l,t))=screen
         x,y=coordinate
-        return x>=l and x<=l+w and y>=t and y<=t+h
+        return x>l and x<l+w and y>t and y<t+h
 
     def __init__(self,selfAddr:tuple):
         self.selfAddr=str(selfAddr)
