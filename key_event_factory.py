bf4f37c336ec0dc1b48a62471e5e0803f36d45f6
Roa-Haskel <10317199+Roa-Haskel@user.noreply.gitee.com>
2022-10-27 21:06:03 +0800

    key event factory 4

--- key_event_factory.py
+++ key_event_factory.py
@@ -25,8 +25,8 @@ class KeyEventFactory:
     keyNames="""
 cmd:alt
 alt_l:cmd
-""".strip().split(":")
-    keyNames={i[0]:i[1] for i in keyChars}
+""".strip().split("\n")
+    keyNames={i.split(":")[0]:i.split(":")[1] for i in keyNames}
 
     keyChars={i[0]:i[1] for i in keyChars}
     def __init__(self):
